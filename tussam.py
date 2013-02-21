47e66e97952802039655adf8306d7908970ba783
javier1973 <fjjaviergv@yahoo.es>
2013-02-21 16:56:18 +0100

    primer intento

--- tussam.py
+++ tussam.py
@@ -1,1 +1,7 @@
-este es la api de tuusam
+﻿#este es la api de tussam
+#realizamos una petición a la api de tussam con la line y el numero de parada
+#nos devuelve el dos autobuses mas cercanos con distancia y tiempo estimado para
+#llegar a la parada
+from suds.client import Client
+cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl ')
+print cliente.service.GetPasoParada ("26","357",1)
