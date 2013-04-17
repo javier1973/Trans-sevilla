12e8ac68d60a408dee5de37b7903ab4234d19ed0
javier1973 <fjjaviergv@yahoo.es>
2013-04-17 23:47:17 +0200

    algunas modificaciones, ya consulto y listo el de las bicis, tussam aún no

--- bicis.py
+++ bicis.py
@@ -1,7 +1,13 @@
+﻿from urllib2 import urlopen
+from json import load
+
+urlt = 'http://api.citybik.es/sevici.json'
+bicis = load(urlopen(urlt))
+#print len(bicis)
 
 
 
-import requests
-r = requests.get("http://api.citybik.es/sevici.json")
-print r.text
-
+for i in bicis:
+    print i.get('name'),'Identificador:' ,'Número',i.get('number'),'Nº bicis' ,i.get('bikes'),'Libres:',i.get('free')
+    print i.get('lat'), i.get('lng')
+#print bicis[1].get('name'),'Número :', bicis[1].get('number'), bicis[1].get('bikes'),bicis[1].get('free') 
--- tussam.py
+++ tussam.py
@@ -1,28 +1,46 @@
 ﻿from suds.client import Client
+from lxml import etree
+from lxml.html import soupparser
 #este es la api de tussam
+
+
+# Para realizar consultas de datos dinámicos
+#cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl',retxml=True)
+
 #realizamos una petición a la api de tussam con la line y el numero de parada
 #nos devuelve el dos autobuses mas cercanos con distancia y tiempo estimado para
 #llegar a la parada
-
-cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl ')
-print cliente.service.GetPasoParada ("26","357",1)
+#print cliente.service.GetPasoParada ("12","974",1)
 
 
+# Para realizar consultas de datos estaticos
+cliente = Client('http://www.infobustussam.com:9001/services/estructura.asmx?wsdl',retxml=True)
 
 #de esta forma obtenemos listado de todas las peticiones que podemos hacer a la url estatica 
-cliente = Client('http://www.infobustussam.com:9001/services/estructura.asmx?wsdl ')
-print cliente
+#print cliente
 
 #De esta froma podemos consultar los nodos(paradas) de una linea
-cliente = Client('http://www.infobustussam.com:9001/services/estructura.asmx?wsdl ')
-print cliente.service.GetNodosMapSublinea ("01",1)
+#print cliente.service.GetNodosMapSublinea ("12",1)
 
 
 #Conseguimos listado de las lineas actuales
-cliente = Client('http://www.infobustussam.com:9001/services/estructura.asmx?wsdl ')
-print cliente.service.GetLineas()
 
+lineas = cliente.service.GetLineas()
+print ('lineas es del tipo: ',type(lineas))
+#lin = soupparser.fromstring(lineas)
+lin = etree.fromstring(lineas)
+print ('lin :', type (lin))
+#linea = etree.XMLID(lineas)
+#linea = etree.ElementTree(element=lin)
+#print linea
 
+print etree.tostring(lin , pretty_print = True)
+#print ('lin es del tipo: ',type(lin))
+#print ('linea es del tipo: ',type(linea))
+#ln = lin.xpath("/soap:Envelope/soap:Body/GetLineasResponse/GetLineasResult/InfoLinea/sublineas/InfoSublinea")
+#print ('ln es del tipo: ',type(ln))
+#for i in lin :
+#    print 
+    
 #De esta forma obtenemos coordenadas del trazado de una linea
-cliente = Client('http://www.infobustussam.com:9001/services/estructura.asmx?wsdl ')
-print cliente.service.GetPolylineaSublinea ("13",1)
+#print cliente.service.GetPolylineaSublinea ("13",1)
